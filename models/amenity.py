b73cb4a80884421df259ac54c2f3e4585b5b2a28
Christian Tonny <g.iradukund1@alustudent.com>
2025-05-20 11:12:18 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -9,7 +9,9 @@ import os
 class Amenity(BaseModel, Base):
     """Amenity class that inherits from BaseModel and Base"""
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False, default="")
+    name = Column(String(128), nullable=False)
+    # The place_amenities relationship will be created by the backref
+    # from Place.amenities
 
     def __init__(self, *args, **kwargs):
         """Initializes Amenity instance"""
